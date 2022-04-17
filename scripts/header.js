e2e46f6899185b46ebc0869171b2df327c5f8432
abharathore06 <aabharathore86862@gmail.com>
2022-04-17 16:15:51 +0530

    Homepage done

---
+++ scripts/header.js
@@ -0,0 +1,21 @@
+// const headerCode = document.createElement('template');
+// headerCode.innerHTML = `
+//     <header>
+//         <h1>ScriBBler</h1>
+//         <p>Explore, Imagine, Create</p>
+//         <button type="button" >Sign In</button>
+//         <button type="button" >Sign Up</button>
+//     </header>
+// `;
+
+
+// class Header extends HTMLElement{
+//     constructor(){
+//         super();
+//     }
+//     connectedCallBack(){
+//         const shadowRoot = this.attachShadow({mode: 'open'});
+//         shadowRoot.appendChild(headerCode.content);
+//     }
+// }
+// customElements.define('header-code', Header);
